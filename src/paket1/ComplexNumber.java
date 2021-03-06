09423da30854b84fc11154fbe7a6d850c4c01a63
yaseoz <64523146+yaseoz@users.noreply.github.com>
2021-03-05 21:13:47 -0500

    Ilk repo deneme

---
+++ src/paket1/ComplexNumber.java
@@ -0,0 +1,11 @@
+package paket1;
+
+public class ComplexNumber {
+    int i;
+    double b;
+    public static void main(String[] args) {
+
+        System.out.println("jjjjjjj");
+
+    }
+}
---
+++ src/paket1/Main.java
@@ -0,0 +1,10 @@
+package paket1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+	    // selam..........
+        //some updates accured
+    }
+}
