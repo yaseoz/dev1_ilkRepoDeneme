409d0aec44d45eee432299fd5fd3550a605c8461
yaseoz <64523146+yaseoz@users.noreply.github.com>
2021-03-08 00:44:45 -0500

    Merge branch 'kaan_ile_branch' of /Users/yso/SpringBoot/dev1_ilkRepoDeneme with conflicts.

--- src/paket1/MathLib.java
+++ src/paket1/MathLib.java
@@ -4,18 +4,6 @@ public class MathLib {
 
     public static void main(String[] args) {
         System.out.println("hello");
-<<<<<<< Updated upstream
-=======
-
-        // selam
     }
->>>>>>> Stashed changes
 
-        // selam
-
-        // deneme 2
-
-
-
-    }
 }
