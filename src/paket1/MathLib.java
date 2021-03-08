aafd8f70d5a821af50e6ff427fb7e4b5300974a1
yaseoz <64523146+yaseoz@users.noreply.github.com>
2021-03-08 00:36:37 -0500

    after stash pop

--- src/paket1/MathLib.java
+++ src/paket1/MathLib.java
@@ -4,6 +4,12 @@ public class MathLib {
 
     public static void main(String[] args) {
         System.out.println("hello");
+<<<<<<< Updated upstream
+=======
+
+        // selam
+    }
+>>>>>>> Stashed changes
 
         // selam
 
